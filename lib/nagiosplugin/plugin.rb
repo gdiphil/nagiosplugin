a5b339afb65724568326de99a84d9ac8e696d2af
Björn Albers <bjoernalbers@googlemail.com>
2012-04-02 14:03:37 +0200

    Move StatusError with examples into separate files

--- lib/nagiosplugin/plugin.rb
+++ lib/nagiosplugin/plugin.rb
@@ -1,36 +1,5 @@
 module NagiosPlugin
   class Plugin
-
-    # A custom status error which will be raised through the status methods.
-    class StatusError < StandardError
-
-      # All allowed statuses sorted by their corresponding exit status.
-      STATUS = [:ok, :warning, :critical, :unknown]
-
-      # @param [Symbol] status the status (must be {NagiosPlugin::Plugin::StatusError::STATUS a valid status})
-      # @param [String] message the message you want to display
-      def initialize(status, message)
-        @status, @message = status.to_sym, message
-      end
-
-      # @return [String] the status and message
-      def to_s
-        "#{status.to_s.upcase}: #{@message}"
-      end
-
-      # @return [Fixnum] the status converted into an exit code
-      def to_i
-        STATUS.find_index(status)
-      end
-
-    private
-
-      # @return [Symbol] the status (:unknown if invalid)
-      def status
-        (STATUS.include?(@status) && @status) || STATUS.last
-      end
-    end
-
     class << self
 
       # Create new instance and run it.
@@ -58,7 +27,7 @@ module NagiosPlugin
 
     # Overwrite this check method and call a status method within.
     def check
-      unknown 'please overwrite the method `check` in your class'
+      unknown 'please overwrite the `check` method in your class'
     end
 
    # Run check and return result in a nagios-compatible format.

eca15e1be834463899c77a794eea098a34902bec
Björn Albers <bjoernalbers@googlemail.com>
2012-04-02 22:36:48 +0200

    Fix indentation

--- lib/nagiosplugin/plugin.rb
+++ lib/nagiosplugin/plugin.rb
@@ -30,22 +30,22 @@ module NagiosPlugin
       unknown 'please overwrite the `check` method in your class'
     end
 
-   # Run check and return result in a nagios-compatible format.
-   #
-   # It will...
-   # - execute your check method
-   # - output the result in a nagios-compatible format (SERVICE STATUS: Message)
-   # - exit according to the status
-   def run
-     check
-   rescue StatusError => e
-   rescue => e
-     e = StatusError.new(:unknown, ([e.to_s, nil] + e.backtrace).join("\n"))
-   else
-     e = StatusError.new(:unknown, 'no status method was called')
-   ensure
-     puts [self.class.name.upcase, e.to_s].join(' ')
-     exit e.to_i
-   end
+    # Run check and return result in a nagios-compatible format.
+    #
+    # It will...
+    # - execute your check method
+    # - output the result in a nagios-compatible format (SERVICE STATUS: Message)
+    # - exit according to the status
+    def run
+      check
+    rescue StatusError => e
+    rescue => e
+      e = StatusError.new(:unknown, ([e.to_s, nil] + e.backtrace).join("\n"))
+    else
+      e = StatusError.new(:unknown, 'no status method was called')
+    ensure
+      puts [self.class.name.upcase, e.to_s].join(' ')
+      exit e.to_i
+    end
   end
 end

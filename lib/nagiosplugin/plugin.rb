06b5f0dd2e31def91113c82dd103925ca0ad5021
Björn Albers <bjoernalbers@googlemail.com>
2012-04-02 22:47:41 +0200

    Add service method

--- lib/nagiosplugin/plugin.rb
+++ lib/nagiosplugin/plugin.rb
@@ -30,6 +30,17 @@ module NagiosPlugin
       unknown 'please overwrite the `check` method in your class'
     end
 
+    # Return the service name, which will be displayed on stdout.
+    #
+    # By default this will be the upcased class name. If you want
+    # to name your service different than the class then overwrite
+    # this method.
+    #
+    # @return [String] the service name
+    def service
+      self.class.name.upcase
+    end
+
     # Run check and return result in a nagios-compatible format.
     #
     # It will...
@@ -44,7 +55,7 @@ module NagiosPlugin
     else
       e = StatusError.new(:unknown, 'no status method was called')
     ensure
-      puts [self.class.name.upcase, e.to_s].join(' ')
+      puts [service, e.to_s].join(' ')
       exit e.to_i
     end
   end
